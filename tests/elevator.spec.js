8806151b0d8386d96117ebf088ac8ea17ec14992
Javier Gonzalez <javier.e.glz@gmail.com>
2020-06-30 09:40:05 -0500

    add going up test

--- tests/elevator.spec.js
+++ tests/elevator.spec.js
@@ -7,3 +7,25 @@ test("Should start on the ground floor or the given floor passed to the construc
     expect( elevator.floor ).toBe(15);
 
 })
+
+test("Should increase the floor count when going up and decrease when going down", () => {
+    let elevator = new Elevator();
+    elevator.goUp()
+    expect( elevator.floor ).toBe(1);
+    elevator.goUp()
+    expect( elevator.floor ).toBe(2);
+    elevator.goDown()
+    expect( elevator.floor ).toBe(1);
+})
+
+test("Should travel to a given floor by going to all the floors in between.", () => {
+    let elevator = new Elevator();
+    elevator.goToFloor(7);
+    expect( elevator.floor ).toBe(7);
+
+    elevator.goToFloor(7);
+    expect( elevator.floor ).toBe(7);
+
+    elevator.goToFloor(2)
+    expect( elevator.floor ).toBe(2);
+})
