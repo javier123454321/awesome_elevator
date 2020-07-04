81d9b00cdcc891521886e86d21b7d73deca725e7
Javier Gonzalez <javier.e.glz@gmail.com>
2020-07-04 02:27:32 -0500

    cleaning up some functions, removing logs

--- src/functions/buttons-up-down.js
+++ src/functions/buttons-up-down.js
@@ -2,31 +2,28 @@ import { watchIsOnFloor } from '../functions/watch-for-changes.js';
 import { renderOpenElevator } from '../functions/renderFunctions.js'
 
 export default function upDownButtonsSetup(elevator, userFloor){
-
     let buttons = document.querySelectorAll('.button');
     buttons.forEach(item => {
         item.removeEventListener('click', onButtonClick);
         item.addEventListener('click', ()=>{ 
             onButtonClick(elevator, userFloor, item)
         })})
+
     watchIsOnFloor(elevator, userFloor, renderOpenElevator);
 }
 
 function onButtonClick(elevator, userFloor, button){
     if(elevator.floor == userFloor){
         button.classList.add('active');
-        console.log('user is at destination');
         renderOpenElevator() 
         return
     }
 
-    button.removeEventListener('click', onButtonClick, false)
     if(document.getElementsByClassName('active').length === 0){
         elevator.goToFloor(userFloor, () => {
             document.getElementById('floor-number').innerHTML = elevator.floor;
         });
     }
-    if(!button.classList.contains('active')){
-        button.classList.add('active');
-    }
+    
+    button.classList.add('active');
 }
--- src/functions/renderFunctions.js
+++ src/functions/renderFunctions.js
@@ -4,13 +4,20 @@ import Elevator from '../classes/Elevator.js';
 
 function renderOpenElevator(){
     if(isElevatorShowing() || upOrDownButtonIsActive()){
-        document.getElementById('elevator').classList.replace('transparent', 'opaque');
-        document.getElementById('elevator').classList.replace('hidden', 'shown')
-        document.getElementById('up-down').classList.replace('opaque','transparent'); 
         renderElevatorButtons()
     }
     return
 }
+
+function renderClosedElevator(){
+    
+    removeActiveClassesFromButtons();
+
+    addHiddenElevatorStyles();
+
+    renderUpDownButtons(getCurrentElevatorState(), getCurrentUserState());
+    return
+} 
 
 function getCurrentElevatorState(){
     let elevator = new Elevator( document.getElementById('floor-number').innerText )
@@ -29,19 +36,17 @@ function renderUpDownButtons(elevator = getCurrentElevatorState(), userFloor = 0
     upDownButtonsSetup(elevator, userFloor)
 }
 
-function renderClosedElevator(){
-        Array.from(document.getElementsByClassName('button')).forEach(element => {
-            element.classList.remove('active')
-        });
+function removeActiveClassesFromButtons() {
+    Array.from(document.getElementsByClassName('button')).forEach(element => {
+        element.classList.remove('active');
+    });
+}
 
-        let elev = document.getElementById('elevator')
-        elev.classList.replace('opaque', 'transparent');
-        elev.classList.add('hidden');
-
-        document.getElementById('up-down').classList.replace('transparent', 'opaque');
-        renderUpDownButtons(getCurrentElevatorState(), getCurrentUserState());
-        return
-    } 
+function addHiddenElevatorStyles() {
+    let elev = document.getElementById('elevator');
+    elev.classList.add('hidden');
+    document.getElementById('up-down').classList.replace('transparent', 'opaque');
+}
 
 function isElevatorShowing(){
     let elevatorClasses = document.getElementById('elevator').classList
@@ -57,7 +62,9 @@ function upOrDownButtonIsActive(){
 }
 
 function renderElevatorButtons(){
+    addShownElevatorStyles(); 
     let floorPickerButtonsArea = document.getElementById('pick-floor');
+
     if(document.getElementsByClassName('pickFloorButton').length > 13){
         floorPickerButtonsArea.classList.replace('transparent', 'opaque');
         return
@@ -65,22 +72,31 @@ function renderElevatorButtons(){
     floorPickerButtonsArea.classList.add('shown')
     floorPickerButtonsArea.classList.replace('transparent', 'opaque');
     
-    for(let i = -3; i <= 13; i++){
-        let button =  document.createElement('button');
-        button.classList.add('pickFloorButton')
-        button.classList.add('button')
-        button.innerText = i
-        floorPickerButtonsArea.appendChild( button );
-        button.addEventListener('click', () => {
-            setElevatorButtonListener(button, i)
-        })
-    }
+    createElevatorFloorButtons(floorPickerButtonsArea, 13, -3);
     return
+}
+
+function createElevatorFloorButtons(floorPickerButtonsArea, number, start = 0) {
+    for (let i = start; i <= number; i++) {
+        let button = document.createElement('button');
+        button.classList.add('pickFloorButton');
+        button.classList.add('button');
+        button.innerText = i;
+        floorPickerButtonsArea.appendChild(button);
+        button.addEventListener('click', () => {
+            setElevatorButtonListener(button, i);
+        });
+    }
+}
+
+function addShownElevatorStyles() {
+    document.getElementById('elevator').classList.replace('transparent', 'opaque');
+    document.getElementById('elevator').classList.replace('hidden', 'shown');
+    document.getElementById('up-down').classList.replace('opaque', 'transparent');
 }
 
 function setElevatorButtonListener(button, targetFloor){
     if( button.classList.contains('active') ){
-        console.log(`ignoring, because already pressed`);
         return
     }
     
@@ -89,10 +105,12 @@ function setElevatorButtonListener(button, targetFloor){
     let userCurrentFloorNode = document.getElementById('user-current-floor');
     
     let elevator = elevatorSetup(floorNumberNode, userCurrentFloorNode, floorNumberNode.innerHTML, userCurrentFloorNode.innerHTML);
+    
     elevator.goToFloor(targetFloor, () => {
         floorNumberNode.innerHTML = elevator.floor;
         userCurrentFloorNode.innerHTML = elevator.floor;
     })
+
     let areWeThereYet = setInterval(() => {
         if( elevator.floor == targetFloor ){
             button.classList.remove('active');
